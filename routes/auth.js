37977b5980b99eefadf24f2a658d09bf8076876f
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-09-23 15:16:45 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,13 @@ const router = express.Router();
 
 
 // SIGN UP
-router.get('/verify/:verificationToken', ctrl.verifyEmail);                       // запит на верифікацію єлектронної пошти юзера   
-router.post('/signup', validateBody(schemas.signUpSchema), ctrl.signup);          //запит на реєстрацію нового користувача
-router.post('/verify', validateBody(schemas.emailSchema), ctrl.resendVerifyEmail);// запит на повторну верифікацію єлектронної пошти юзера
+router.get('/verify/:verificationToken', ctrl.verifyEmail);                         // запит на верифікацію єлектронної пошти юзера   
+router.post('/signup', validateBody(schemas.signUpSchema), ctrl.signup);            //запит на реєстрацію нового користувача
+router.post('/verify', validateBody(schemas.emailSchema), ctrl.resendVerifyEmail);  // запит на повторну верифікацію єлектронної пошти юзера
 
 // SIGN IN
-router.post('/signin', validateBody(schemas.signInSchema), ctrl.signin);           //запит на авторизацію існуючого користувача
-router.post('/signout',authenticate, ctrl.signout);                               //запит на розавторизацію існуючого користувача
+router.post('/signin', validateBody(schemas.signInSchema), ctrl.signin);            //запит на авторизацію існуючого користувача
+router.post('/signout',authenticate, ctrl.signout);                                 //запит на розавторизацію існуючого користувача
 
 
     
