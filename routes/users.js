6b1d1b82ca7c0fcccf905841f525d134771c9bbc
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-09-28 21:46:22 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // SIGN IN --------------------------------------------------------------------------------------------------------------------------
 
 router.get('/current', authenticate, ctrl.getCurrent);                            // + запит на отримання інформації про поточного користувача
-router.patch('/update', authenticate, upload.single("avatar"), ctrl.updateUser);  // + запит на оновлення профайлу поточного юзера
+router.patch('/update', authenticate, ()=>{console.log("Before upload"); upload.single("avatar"); console.log("After upload")}, ctrl.updateUser);  // + запит на оновлення профайлу поточного юзера
 router.post('/subscribe',authenticate, ctrl.subscribe);                           // + надсилання листа з повідомленням про підписку на розсилку
 
 // ----------------------------------------------------------------------------------------------------------------------------------
