60a7d7b3d3a05734f4fd8476ec361009544c5739
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-09-30 21:03:02 +0300

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -6,6 +6,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true,
 });
 
 const storage = new CloudinaryStorage({
