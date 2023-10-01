f23f4b485b935504685ffba156a2d79cf7914e41
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-10-01 13:44:07 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -18,28 +18,22 @@ const {SECRET_KEY, BASE_URL} = process.env;
 
 
 //+ оновлення даних про поточного користувача (можемо оновити або аватар, або ім'я юзера - user profile window)
-  //const avatarsDir = path.join(__dirname, "../", "public", "avatars");   //!!!!змінити на cloudinary
-    const updateUser  = async(req, res) => {
+  //const avatarsDir = path.join(__dirname, "../", "public", "avatars");   //!!!!змінити на cloudinary  const updateUser  = async(req, res) => {
     console.log("---- updateUser function ---- ");
     let newAvatarURL;
 
     const {_id, currentAvatarURL, name: currentUserName} = req.user;              // забираємо id поточного юзера
-    const {name = currentUserName} = req.body;                                    // забираємо нове ім'я поточного юзера з http-запиту
+    const {name: newUserName} = req.body;                                    // забираємо нове ім'я поточного юзера з http-запиту
     
-    console.log("req.file = ", req.file);
+    if (!newUserName) {newUserName = currentUserName}
 
-    if (req.file) { 
-      newAvatarURL = req.file.path;  
-      console.log("newAvatarURL = req.file.path =", newAvatarURL);
-    }
-    else { 
-      console.log("newAvatarURL =currentAvatarURL =", newAvatarURL);
-      newAvatarURL = currentAvatarURL; 
-    }
-    console.log("я перед findByIdAndUpdate =");
+    if (req.file) { newAvatarURL = req.file.path;  }
+    else { newAvatarURL = currentAvatarURL; }
+
     const usr = await User.findByIdAndUpdate(_id, {name, avatarURL: newAvatarURL}, {new: true}); // оновлюємо поле avatarURL для поточного юзера
     res.json({name: usr.name, avatarURL: usr.avatarURL });
   }
+ 
 
 
 //+ надсилання листа з повідомленням про підписку на розсилку
