968964c25d960da10ee253d9395f6b03627214a8
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-10-01 12:28:49 +0300

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,4 +1,4 @@
-console.log("я в начале upload", newAvatarURL);
+console.log("я в начале upload");
 
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
