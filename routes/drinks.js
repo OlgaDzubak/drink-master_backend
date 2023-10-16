bb76403368c90bb206470cad299cde576bd72a2d
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-10-16 10:33:12 +0300

    Update drinks.js
    Fixing

--- routes/drinks.js
+++ routes/drinks.js
@@ -7,18 +7,18 @@ const router = express.Router();
 
 //------------------------------------------------------------------------------------------------
 
-router.get('/mainpage', authenticate, ctrl.getDrinksForMainPage);                               //+
-router.get('/own', authenticate, ctrl.getAllDrinks);                                            //+
-router.get('/popular', authenticate, ctrl.getPopularDrinks);                                    //+
-router.get('/search', authenticate, ctrl.searchDrinks);                                         //+
-router.get('/favorite', authenticate, ctrl.getFavoriteDrinks);                                  //+
-router.get('/:id', authenticate, validateId, ctrl.getDrinkById);                                //+
+router.get('/mainpage', authenticate, ctrl.getDrinksForMainPage);                               // маршрут для отримання напоїв для головної сторінки
+router.get('/own', authenticate, ctrl.getAllDrinks);                                            // маршрут для отримання власних напоїв
+router.get('/popular', authenticate, ctrl.getPopularDrinks);                                    // маршрут для отримання популярних напоїв
+router.get('/search', authenticate, ctrl.searchDrinks);                                         // маршрут для пошуку напоїв
+router.get('/favorite', authenticate, ctrl.getFavoriteDrinks);                                  // маршрут для отримання всіх своїх улюблених напоїв 
+router.get('/:id', authenticate, validateId, ctrl.getDrinkById);                                // маршрут для отримання напою по його id
 
-router.post('/own/add', authenticate, upload.single("drinkThumb"), ctrl.addDrink);              //+
-router.post('/favorite/add/:id', authenticate, validateId,ctrl.addDrinkToFavorite);             //+ !!! СПИТАТИ У МЕНТОРА ЯК ПЕРЕДАВАТИ ID, ЯК ПАРАМЕТР ЧИ В BODY
+router.post('/own/add', authenticate, upload.single("drinkThumb"), ctrl.addDrink);              // маршрут для додавання власного напою
+router.post('/favorite/add/:id', authenticate, validateId,ctrl.addDrinkToFavorite);             // маршрут для додавання напою в улюблені
 
-router.delete('/own/remove/:id', authenticate, validateId, ctrl.deleteDrinkById);                          //+
-router.delete('/favorite/remove/:id', authenticate, validateId, ctrl.removeDrinkFromFavorite);  //+ !!! СПИТАТИ У МЕНТОРА ЯК ПЕРЕДАВАТИ ID, ЯК ПАРАМЕТР ЧИ В BODY
+router.delete('/favorite/remove/:id', authenticate, validateId, ctrl.removeDrinkFromFavorite);  // маршрут для видалення напою з улюблених
+router.delete('/own/remove/:id', authenticate, validateId, ctrl.deleteDrinkById);               // маршрут для видалення власного напою
 
 //------------------------------------------------------------------------------------------------
 
