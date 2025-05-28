380198636058c5a10e930aefaeb9f4066a5e4ec3
Olga Dzubak <iamAndriy80@gmail.com>
2025-05-28 12:40:00 +0300

    Update drinks.js

--- routes/drinks.js
+++ routes/drinks.js
@@ -13,7 +13,7 @@ router.get('/search', authenticate, ctrl.searchDrinks);
 router.get('/favorite', authenticate, ctrl.getFavoriteDrinks);
 router.get('/:id', validateId, ctrl.getDrinkById);
 
-router.post('/own/add', authenticate, validateBody(schemas.addSchema), upload.single("drinkThumb"), ctrl.addDrink);
+router.post('/own/add', authenticate, upload.single("drinkThumb"), ctrl.addDrink);
 router.post('/favorite/add/:id', authenticate, validateId, ctrl.addDrinkToFavorite);
 
 router.delete('/own/remove/:id', authenticate, validateId, ctrl.deleteDrinkById);
